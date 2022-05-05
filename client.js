a8d9c69c942f0d3e3e1f41602e3e367a764cafa2
Haseeb <23100044@lums.edu.pk>
2022-05-05 13:07:12 +0500

    updated

--- client.js
+++ client.js
@@ -4,22 +4,148 @@ const Sequence = () => {
   const [board, setBoard] = React.useState([[]]);
   const [positionBoard, setPositionBoard] = React.useState([[]]);
   const [cards, setCards] = React.useState([]);
+  const [deck, setDeck] = React.useState([]);
+  const [message, setMessage] = React.useState("Add message here.");
+  const [clientID, setClientID] = React.useState(1);
+  const [colour, Setcolour] = React.useState();
 
   let diamondSign = "♦";
   let heartSign = "♥";
   let spadesSign = "♠";
   let clubsSign = "♣";
+  ws.onopen = () => {};
+  let counter = 0;
+  let client_id;
+  ws.onmessage = function (event) {
+    let message_from_server = JSON.parse(event.data);
+    let msgtype = message_from_server.msgtype;
+    if (msgtype == "starting") {
+      let message_from_server = JSON.parse(event.data);
+      let board = message_from_server.board;
+      let positionBoard = message_from_server.positionBoard;
+      let deck = message_from_server.deck;
+      let colour = message_from_server.colour;
+      setClientID(message_from_server.client);
+      setPositionBoard(positionBoard);
+      setDeck(deck);
+      Setcolour(colour);
+      setBoard(board);
+    } else if (msgtype == "updatepboard") {
+      let message_from_server = JSON.parse(event.data);
+      let positionBoard = message_from_server.positionBoard;
+      setPositionBoard(positionBoard);
+    } else if (msgtype == "updatedeck") {
+      let message_from_server = JSON.parse(event.data);
+      let newdeck = message_from_server.deck;
+      setDeck(newdeck);
+    } else if (msgtype == "setMessage") {
+      let message_from_server = JSON.parse(event.data);
+
+      setMessage(message_from_server.msg);
+    } else if (msgtype == "updatecards") {
+      let message_from_server = JSON.parse(event.data);
+      setDeck(message_from_server.deck);
+    }
+  };
+  ws.onclose = function () {};
+
+  function iscardback(str) {
+    let string = str.split(" ");
+    if (string[string.length - 1] == "back") {
+      return true;
+    }
+    return false;
+  }
+
+  function cardnum(str) {
+    let string = str.split(" ");
+    let str2 = string[1].split("-");
+    return str2[1];
+  }
+
+  function signreturner(str) {
+    let string = str.split(" ");
+    if (string[string.length - 1] == "spades") {
+      return spadesSign;
+    } else if (string[string.length - 1] == "diams") {
+      return diamondSign;
+    } else if (string[string.length - 1] == "clubs") {
+      return clubsSign;
+    } else {
+      return heartSign;
+    }
+  }
+
+  function checkifpresent(str, row, column) {
+    let obj = {
+      clientID: clientID,
+      row: row,
+      column: column,
+      msgtype: "myturn",
+      card: str,
+    };
+    ws.send(JSON.stringify(obj));
+  }
 
   return (
     <div>
-      <div class="container">{/* code for sequence board comes here */}</div>
+      <div class="container">
+        {board.map((row, rowId) => (
+          <div class="playingCards fourColours rotateHand">
+            <ul class="table">
+              {row.map((element, columnID) => (
+                <div>
+                  <li>
+                    {iscardback(element) ? (
+                      <div class="card back">
+                        <span class="rank"></span>
+                      </div>
+                    ) : positionBoard[columnID][rowId] == "g" ? (
+                      <div className="card">
+                        <div className="green"></div>
+                      </div>
+                    ) : positionBoard[columnID][rowId] == "b" ? (
+                      <div className="card">
+                        <div className="blue"></div>
+                      </div>
+                    ) : (
+                      <div
+                        class={element}
+                        onClick={() => checkifpresent(element, columnID, rowId)}
+                      >
+                        <span class="rank">{cardnum(element)}</span>
+                        <span class="suit">{signreturner(element)}</span>
+                      </div>
+                    )}
+                  </li>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+
       <div class="container">
         <div>
           <h1>Your Cards:</h1>
         </div>
-        {/* code for client cards comes here */}
-        {/* code for text box comes here */}
-        {/* code for circle representing the players team color comes here */}
+        {
+          <div class="playingCards fourColours rotateHand">
+            <ul class="table">
+              {deck.map((each) => (
+                <li>
+                  <a class={each}>
+                    <span class="rank">{cardnum(each)}</span>
+                    <span class="suit">{signreturner(each)}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        }
+        {<div className="text_box"> {message} </div>}
+
+        {<div className={`color ${colour}`}></div>}
       </div>
     </div>
   );
--- server.js
+++ server.js
@@ -246,5 +246,315 @@ const server = http.createServer(async (req, resp) => {
 // to listen for clients
 server.listen(8000);
 
-// creating a web socket
 const wss = new WebSocket.Server({ port: 8080 });
+const clients = new Map();
+let sentagain = false;
+let clientID = 0;
+let divideddeck = divideDeckIntoPieces(deck);
+let turn = 1;
+wss.on("connection", (ws) => {
+  clientID += 1;
+  const id = clientID;
+  const clientInfo = id;
+  clients.set(ws, clientInfo);
+  // console.log("aaaaaaaaaaaaaaaaaaaaa");
+  if (clientID == 4) {
+    let counter = 0;
+    for (let client of clients.keys()) {
+      let obj = {
+        client: clients.get(client),
+        board: board,
+        type: "newboard",
+        positionBoard: positionBoard,
+        deck: divideddeck[counter],
+        msgtype: "starting",
+        colour: counter % 2 == 0 ? "green" : "blue",
+      };
+
+      // console.log("LOOOK HERERER");
+      // console.log(clients.get(client));
+      counter++;
+      client.send(JSON.stringify(obj));
+    }
+    setmessage("Your turn", 1);
+  }
+
+  ws.on("message", (msgString) => {
+    let message = JSON.parse(msgString.toString());
+    // console.log(message);
+    // console.log(message.msgtype);
+    if (message.msgtype == "myturn") {
+      if (turn == message.clientID) {
+        let deck_index = turn - 1;
+        if (sentagain) {
+          deck_index += 4;
+        }
+        let card = message.card;
+        let present = divideddeck[deck_index].includes(card);
+        let currplayerdeck = divideddeck[deck_index];
+        // console.log(divideddeck[deck_index]);
+        // console.log(divideddeck[deck_index].includes(card));
+
+        if (present) {
+          let column = message.column;
+          let row = message.row;
+          // console.log(row, column);
+          positionBoard[row][column] = deck_index % 2 == 0 ? "g" : "b";
+          ////////////////////////////////
+          newdeck = [];
+          for (let i = 0; i < divideddeck[deck_index].length; i++) {
+            if (divideddeck[deck_index][i] == card) {
+            } else {
+              newdeck.push(divideddeck[deck_index][i]);
+            }
+          }
+          // console.log(newdeck);
+          divideddeck[deck_index] = newdeck;
+          updateboardanddeck(turn, newdeck);
+        } else if (jackchecker(currplayerdeck)) {
+          // console.log("verynice");
+          let column = message.column;
+          let row = message.row;
+          positionBoard[row][column] = deck_index % 2 == 0 ? "g" : "b";
+          // console.log("Very nice");
+          let indexremove = jackindexreturner(currplayerdeck);
+          // console.log(indexremove);
+          let newdeck = [];
+          for (let i = 0; i < divideddeck[deck_index].length; i++) {
+            if (i == indexremove) {
+            } else {
+              newdeck.push(divideddeck[deck_index][i]);
+            }
+          }
+          divideddeck[deck_index] = newdeck;
+          updateboardanddeck(turn, newdeck);
+        } else {
+          // console.log("invalid move");
+          setmessage("Invalid move", message.clientID);
+        }
+      } else {
+        // console.log("not your turn");
+        setmessage("not your turn", message.clientID);
+      }
+    }
+  });
+});
+
+// creating a web socket
+
+function getByValue(map, searchValue) {
+  for (let [key, value] of map.entries()) {
+    if (value === searchValue) return key;
+  }
+}
+
+function setmessage(message, turn) {
+  // console.log(turn);
+  let obj = { msgtype: "setMessage", msg: message };
+  let client = getByValue(clients, turn);
+  client.send(JSON.stringify(obj));
+}
+
+function jackchecker(deck) {
+  for (let i = 0; i < deck.length; i++) {
+    let splitted_deck = deck[i].split(" ");
+
+    if (splitted_deck[1] == "rank-j") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function jackindexreturner(deck) {
+  for (let i = 0; i < deck.length; i++) {
+    let splitted_deck = deck[i].split(" ");
+
+    if (splitted_deck[1] == "rank-j") {
+      // console.log("idher dekho");
+      // console.log(i);
+      return i;
+    }
+  }
+
+  // return false;
+}
+
+function updateboardanddeck(turn1, newdeck) {
+  /////////////////////////////////////
+
+  if (newdeck.length == 0 && turn1 == 4) {
+    let counter = 4;
+    // divideddeck
+    for (let client of clients.keys()) {
+      let obj = { deck: divideddeck[counter], msgtype: "updatecards" };
+      client.send(JSON.stringify(obj));
+      let obj1 = { positionBoard: positionBoard, msgtype: "updatepboard" };
+      client.send(JSON.stringify(obj1));
+      counter++;
+    }
+    if (sentagain == false) {
+      sentagain = true;
+    } else {
+      // console.log("DRAWWWWWWWWWW");
+      setmessage("Draw", 1);
+      setmessage("Draw", 2);
+      setmessage("Draw", 3);
+      setmessage("Draw", 4);
+      turn = -1;
+      return;
+    }
+  } else {
+    // console.log(positionBoard);
+    for (let client of clients.keys()) {
+      let obj = { positionBoard: positionBoard, msgtype: "updatepboard" };
+      client.send(JSON.stringify(obj));
+      if (clients.get(client) == turn1) {
+        let obj = { deck: newdeck, msgtype: "updatedeck" };
+        client.send(JSON.stringify(obj));
+      }
+    }
+  }
+
+  let win = winnerchecker();
+  if (win != " ") {
+    if (win == "g") {
+      setmessage("WINNER", 1);
+      setmessage("WINNER", 3);
+    } else {
+      setmessage("WINNER", 2);
+      setmessage("WINNER", 4);
+    }
+    turn = -1;
+    return;
+  }
+  turn = turn1 + 1;
+  turn = turn % 5;
+  if (turn == 0) {
+    turn += 1;
+  }
+  setmessage("Your turn", turn);
+}
+
+function winnerchecker() {
+  // console.log("asdasd");
+  // positionBoard
+
+  for (let i = 2; i < positionBoard.length - 2; i++) {
+    for (let j = 2; j < positionBoard[i].length - 2; j++) {
+      let hor = checkhorizontal(i, j);
+      if (hor != " ") {
+        // console.log("horizontal " + hor + "Winnnnnerrrrrrrrr");
+        return hor;
+      }
+      let ver = checkvertical(i, j);
+      if (ver != " ") {
+        // console.log("vertical " + ver + "Winnnnnerrrrrrrrr");
+        return ver;
+      }
+
+      let diag1 = checkdiagonal1(i, j);
+      if (diag1 != " ") {
+        // console.log("diagonal " + diag1 + "Winnnnnerrrrrrrrr");
+        return diag1;
+      }
+
+      let diag2 = checkdiagonal2(i, j);
+      if (diag2 != " ") {
+        // console.log("diagonal " + diag2 + "Winnnnnerrrrrrrrr");
+        return diag2;
+      }
+    }
+  }
+  return " ";
+}
+
+function checkhorizontal(i, j) {
+  if (
+    positionBoard[i][j] == "g" &&
+    positionBoard[i][j + 1] == "g" &&
+    positionBoard[i][j + 2] == "g" &&
+    positionBoard[i][j - 1] == "g" &&
+    positionBoard[i][j - 2] == "g"
+  ) {
+    return "g";
+  } else if (
+    positionBoard[i][j] == "b" &&
+    positionBoard[i][j + 1] == "b" &&
+    positionBoard[i][j + 2] == "b" &&
+    positionBoard[i][j - 1] == "b" &&
+    positionBoard[i][j - 2] == "b"
+  ) {
+    return "b";
+  } else {
+    return " ";
+  }
+}
+
+function checkvertical(i, j) {
+  if (
+    positionBoard[i][j] == "g" &&
+    positionBoard[i + 1][j] == "g" &&
+    positionBoard[i + 2][j] == "g" &&
+    positionBoard[i - 1][j] == "g" &&
+    positionBoard[i - 2][j] == "g"
+  ) {
+    return "g";
+  } else if (
+    positionBoard[i][j] == "b" &&
+    positionBoard[i + 1][j] == "b" &&
+    positionBoard[i + 2][j] == "b" &&
+    positionBoard[i - 2][j] == "b" &&
+    positionBoard[i - 1][j] == "b"
+  ) {
+    return "b";
+  } else {
+    return " ";
+  }
+}
+
+function checkdiagonal1(i, j) {
+  if (
+    positionBoard[i][j] == "g" &&
+    positionBoard[i + 1][j + 1] == "g" &&
+    positionBoard[i + 2][j + 1] == "g" &&
+    positionBoard[i - 1][j - 1] == "g" &&
+    positionBoard[i - 2][j - 2] == "g"
+  ) {
+    return "g";
+  } else if (
+    positionBoard[i][j] == "b" &&
+    positionBoard[i + 1][j + 1] == "b" &&
+    positionBoard[i + 2][j + 2] == "b" &&
+    positionBoard[i - 2][j - 2] == "b" &&
+    positionBoard[i - 1][j - 1] == "b"
+  ) {
+    return "b";
+  } else {
+    return " ";
+  }
+}
+
+function checkdiagonal2(i, j) {
+  if (
+    positionBoard[i][j] == "g" &&
+    positionBoard[i + 1][j - 1] == "g" &&
+    positionBoard[i + 2][j - 2] == "g" &&
+    positionBoard[i - 1][j + 1] == "g" &&
+    positionBoard[i - 2][j + 2] == "g"
+  ) {
+    return "g";
+  } else if (
+    positionBoard[i][j] == "b" &&
+    positionBoard[i + 1][j - 1] == "b" &&
+    positionBoard[i + 2][j - 2] == "b" &&
+    positionBoard[i - 2][j + 2] == "b" &&
+    positionBoard[i - 1][j + 1] == "b"
+  ) {
+    return "b";
+  } else {
+    return " ";
+  }
+}
+// card rank-j hearts
